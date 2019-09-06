bb260b128674aad9ad58de175b3283b9962e6464
Nick Andujar <na188318@ohio.edu>
2019-09-06 15:07:15 -0400

    Lab program 1st upload

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Nick Andujar
+ *        Date: September 6, 2019
+ * Description: Lab 2 Git Program
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+    cout << "Hello Git" << endl;
+    /*add code*/
+    return 0;
+}// main
